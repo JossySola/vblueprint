298fe9735a3951560a9e2730386f30c9004b3eb6
Jossy Solá <jossysola@gmail.com>
2026-09-12 02:58:52 -0600

    Adds additional types

--- lib/types.ts
+++ lib/types.ts
@@ -1,5 +1,8 @@
+import { ImageConfig } from "konva/lib/shapes/Image";
 import type { CircleConfig } from "konva/lib/shapes/Circle";
+import { PathConfig } from "konva/lib/shapes/Path";
 import type { RectConfig } from "konva/lib/shapes/Rect";
+import { RingConfig } from "konva/lib/shapes/Ring";
 import type { StarConfig } from "konva/lib/shapes/Star";
 import type { TextConfig } from "konva/lib/shapes/Text";
 
@@ -10,9 +13,11 @@ interface ShapeData {
     y: number;
 }
 interface Rect extends RectConfig {
-    type: "rect";
+    type: "rect" | "table" | "wall";
     width: number;
     height: number;
+    fill: string;
+    cornerRadius: number;
 }
 interface Circle extends CircleConfig {
     type: "circle";
@@ -26,4 +31,30 @@ interface Text extends TextConfig {
     text: string;
     fontSize: number;
 }
-export type SHAPE_TYPE = ShapeData & (Rect | Circle | Star | Text);
+interface Ring extends RingConfig {
+    type: "ring";
+    innerRadius: number;
+    outerRadius: number;
+}
+interface Path extends PathConfig {
+    type: "path" | "outfit";
+    data: string;
+    stroke: string;
+}
+interface Wood extends ImageConfig {
+    type: "wood";
+    image: CanvasImageSource | undefined;
+}
+interface Asphalt extends ImageConfig {
+    type: "asphalt";
+    image: CanvasImageSource | undefined;
+}
+interface Brick extends ImageConfig {
+    type: "brick";
+    image: CanvasImageSource | undefined;
+}
+interface Steel extends ImageConfig {
+    type: "steel";
+    image: CanvasImageSource | undefined;
+}
+export type SHAPE_TYPE = ShapeData & (Rect | Circle | Star | Text | Ring | Path | Wood | Asphalt | Brick | Steel);
