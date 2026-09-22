131d8018e6f75d61e0d5d8ae506fbc90772f75d1
Jossy Solá <jossysola@gmail.com>
2026-09-22 00:01:21 -0600

    Updates component

--- app/_components/EditableShape.tsx
+++ app/_components/EditableShape.tsx
@@ -35,20 +35,20 @@ export default function EditableShape({ shape, selected, onSelect, onCommit } : 
 
     return (
         <>
-        {shape.type === 'rect' && <Rect {...shape} {...common} />}
-        {shape.type === 'table' && <Rect {...shape} {...common} />}
-        {shape.type === 'wall' && <Rect {...shape} {...common} />}
-        {shape.type === 'circle' && <Circle {...shape} {...common} />}
-        {shape.type === 'star' && <Star {...shape} {...common} />}
-        {shape.type === 'text' && <Text {...shape} {...common} />}
-        {shape.type === 'path' && <Path {...shape} {...common} />}
-        {shape.type === 'outfit' && <Path {...shape} {...common} />}
-        {shape.type === 'wood' && <Image {...shape} {...common} alt="Image with design texture representing a piece of furniture in the template" />}
-        {shape.type === 'asphalt' && <Image {...shape} {...common} alt="Image with design texture representing a piece of furniture in the template" />}
-        {shape.type === 'brick' && <Image {...shape} {...common} alt="Image with design texture representing a piece of furniture in the template" />}
-        {shape.type === 'steel' && <Image {...shape} {...common} alt="Image with design texture representing a piece of furniture in the template" />}
-        {shape.type === 'brickslight' && <Image {...shape} {...common} crop={crop} alt="Image with design texture representing a piece of furniture in the template" />}
-        {shape.type === 'terrazzo' && <Image {...shape} {...common} alt="Image with design texture representing a piece of furniture in the template" />}
+        {shape.type === 'rect' && <Rect  {...shape} {...common} />}
+        {shape.type === 'table' && <Rect  {...shape} {...common} />}
+        {shape.type === 'wall' && <Rect  {...shape} {...common} />}
+        {shape.type === 'circle' && <Circle  {...shape} {...common} />}
+        {shape.type === 'star' && <Star  {...shape} {...common} />}
+        {shape.type === 'text' && <Text  {...shape} {...common} />}
+        {shape.type === 'path' && <Path  {...shape} {...common} />}
+        {shape.type === 'keylook' && <Path  {...shape} {...common} />}
+        {shape.type === 'wood' && <Image  {...shape} {...common} alt="Image with design texture representing a piece of furniture in the template" />}
+        {shape.type === 'asphalt' && <Image  {...shape} {...common} alt="Image with design texture representing a piece of furniture in the template" />}
+        {shape.type === 'brick' && <Image  {...shape} {...common} alt="Image with design texture representing a piece of furniture in the template" />}
+        {shape.type === 'steel' && <Image  {...shape} {...common} alt="Image with design texture representing a piece of furniture in the template" />}
+        {shape.type === 'brickslight' && <Image  {...shape} {...common} crop={crop} alt="Image with design texture representing a piece of furniture in the template" />}
+        {shape.type === 'terrazzo' && <Image  {...shape} {...common} alt="Image with design texture representing a piece of furniture in the template" />}
         {selected && (
             <Transformer
             ref={transformerRef}
