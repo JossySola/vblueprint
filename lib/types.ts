3a7a6413bba2828fe0923de51acbe86fb7e6cfe3
Jossy Solá <jossysola@gmail.com>
2026-09-22 00:09:07 -0600

    Adds types

--- lib/types.ts
+++ lib/types.ts
@@ -11,60 +11,60 @@ interface ShapeData {
     id: string;
     x: number;
     y: number;
-}
+};
 interface Rect extends RectConfig {
     type: "rect" | "table" | "wall";
     width: number;
     height: number;
     fill: string;
     cornerRadius: number;
-}
+};
 interface Circle extends CircleConfig {
     type: "circle";
     radius: number;
-}
+};
 interface Star extends StarConfig {
     type: "star";
-}
+};
 interface Text extends TextConfig {
     type: "text";
     text: string;
     fontSize: number;
-}
+};
 interface Ring extends RingConfig {
     type: "ring";
     innerRadius: number;
     outerRadius: number;
-}
+};
 interface Path extends PathConfig {
-    type: "path" | "outfit";
+    type: "path" | "keylook";
     data: string;
     stroke: string;
-}
+};
 interface Wood extends ImageConfig {
     type: "wood";
     image: CanvasImageSource | undefined;
-}
+};
 interface Asphalt extends ImageConfig {
     type: "asphalt";
     image: CanvasImageSource | undefined;
-}
+};
 interface Brick extends ImageConfig {
     type: "brick";
     image: CanvasImageSource | undefined;
-}
+};
 interface Steel extends ImageConfig {
     type: "steel";
     image: CanvasImageSource | undefined;
-}
+};
 interface BricksLight extends ImageConfig {
     type: "brickslight";
     image: CanvasImageSource | undefined;
-}
+};
 interface Terrazzo extends ImageConfig {
     type: "terrazzo";
     image: CanvasImageSource | undefined;
-}
+};
 export type SHAPE_TYPE = ShapeData & (
     Rect 
     | Circle 
@@ -79,3 +79,28 @@ export type SHAPE_TYPE = ShapeData & (
     | BricksLight
     | Terrazzo
 );
+export interface CAMERA {
+    x: number;
+    y: number;
+};
+export interface VIEWPORT {
+    width: number;
+    height: number;
+};
+export type GarmentType = {
+    id: string,
+    icon: string,
+    garment: string,
+    material: string,
+    prevLocation: string,
+    currentLocation: string,
+    nextLocation: string,
+    collection?: string,
+};
+export type LocationItems = Map<string, Array<GarmentType>>;
+export type LocationItemsJSON = Array<[string, GarmentType[]]>;
+type DashboardQueryRow = {
+    id: string,
+    title: string,
+};
+export type DashboardQueryResponse = DashboardQueryRow[];
